6a0a4355c3504509ed8406e24439b97008c11220
Piero Badano <pierobadano@outlook.com>
2025-02-15 00:15:09 -0300

    Crea funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
+//  Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById("amigo"); 
+    if (nombreAmigo.value === ""){
+        alert ("Porfavor, Ingrese un nombre válido.");
+        return;
+    }
+    amigos.push(nombreAmigo.value);
+    nombreAmigo.value= "";
+}
